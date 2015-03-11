300f90f10c4c0ba236c33d6a7a54063e54b79ba6
Arpit Mathur <mathur.arpit@gmail.com>
2015-03-10 20:14:20 -0400

    Now caches data into local db

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -4,6 +4,8 @@ class ScheduleController < ApplicationController
 
 	def index
 
+		max_age = 60*60*1000 #1hour
+
 		if(params[:day] == nil)
 			day = 13
 		else
@@ -12,32 +14,47 @@ class ScheduleController < ApplicationController
 
 		day_int = day.to_i
 
-		doc = Nokogiri::HTML(open("http://schedule.sxsw.com/?conference=interactive&day=#{day}"))
-		rows = doc.css("div.eventcol")
-		@events = Array.new
-		ActiveRecord::Base.transaction do
-			rows.each do |row|
-				#puts ("=>>>"+event_id)
-				t = row.at_css("a.link_item")
-				unless(t == nil)
-					event_id =  t['id']
-					loc_node = row.at_css("div.location a")
-					event = Event.new
-					event.event_day = day_int
-					event.event_id = event_id
-					event.title = t.content
-					event.link = t["href"]
-					event.location = loc_node.content
-					time = row.at_css("div.date_time").content.split("-")
-					event.start_time = time[0].chomp
-					event.end_time = time[1].chomp unless time[1] == nil
-					@events << event
-					puts "saving"
-					#event.save
+		last_event = Event.last(:event_day == day_int)
+		now = Time.now
+		if(last_event != nil)
+			puts "HOW OLD >>> #{(now - last_event.created_at)} "
+		end
+
+		if(last_event != nil && ( (now - last_event.created_at) < max_age))
+			puts "Loading from db"
+			evts = Event.where(:event_day == day_int);
+			@events = evts.to_a()
+		else
+			Event.delete_all
+			puts "Scraping site!"
+			doc = Nokogiri::HTML(open("http://schedule.sxsw.com/?conference=interactive&day=#{day}"))
+			rows = doc.css("div.eventcol")
+			@events = Array.new
+			ActiveRecord::Base.transaction do
+				rows.each do |row|
+					#puts ("=>>>"+event_id)
+					t = row.at_css("a.link_item")
+					unless(t == nil)
+						event_id =  t['id']
+						loc_node = row.at_css("div.location a")
+						event = Event.new
+						event.event_day = day_int
+						event.event_id = event_id
+						event.title = t.content
+						event.link = t["href"]
+						event.location = loc_node.content
+						time = row.at_css("div.date_time").content.split("-")
+						event.start_time = time[0].chomp
+						event.end_time = time[1].chomp unless time[1] == nil
+						@events << event
+						event.save
+					end
 				end
 			end
 		end
+
 		@cols = @events.group_by(&:start_time)
-	end
+		@created_at = @events.first.created_at
+		end
 
 end
